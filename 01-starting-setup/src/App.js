dbc34b4e16b50c4f748cbb6e8da116163ecf6f11
Rueben T <tiow.rueben@gmail.com>
2022-05-16 13:52:04 -0700

    Added Expenses Component
    Abstracted the Expenses component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import ExpensesItem from "./components/ExpensesItem";
+import Expenses from "./components/Expenses";
 
 function App() {
 
@@ -25,27 +25,8 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpensesItem 
-        title={expenses[0].title} 
-        amount={expenses[0].amount} 
-        date={expenses[0].date}
-      ></ExpensesItem>
-      <ExpensesItem
-        title={expenses[1].title} 
-        amount={expenses[1].amount} 
-        date={expenses[1].date}
-      ></ExpensesItem>
-      <ExpensesItem
-        title={expenses[2].title} 
-        amount={expenses[2].amount} 
-        date={expenses[2].date}
-      ></ExpensesItem>
-      <ExpensesItem
-        title={expenses[3].title} 
-        amount={expenses[3].amount} 
-        date={expenses[3].date}
-      ></ExpensesItem>
+      <h2>Lets get started!</h2>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
